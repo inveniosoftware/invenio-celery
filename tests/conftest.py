0b079f89ccc71ff4e9a1d6efbe71b2048779948c
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-02-03 22:42:07 +0100

    tests: remove PytestDeprecationWarning
    * replace @pytest.yield_fixture with @pytest.fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -13,12 +14,13 @@ from __future__ import absolute_import, print_function
 
 import sys
 
+import celery._state
 import pytest
 from celery import Celery, shared_task
 from flask import Flask
 
 
-@pytest.yield_fixture()
+@pytest.fixture()
 def app(request):
     """Flask app fixture."""
     app = Flask("testapp")
@@ -38,22 +40,22 @@ def app(request):
         """Dummy function."""
         pass
 
-    yield app
+    def set_celery_state():
+        celery._state._apps.discard(
+            app.extensions["invenio-celery"].celery._get_current_object()
+        )
+        celery._state._on_app_finalizers = set()
+        celery._state.set_default_app(Celery())
 
-    import celery._state
+        # Clear our modules to get them re-imported by Celery.
+        pkgs = [
+            "bpackage.first_tasks",
+            "bpackage.second_tasks",
+            "apackage.third_tasks",
+        ]
+        for pkg in pkgs:
+            if pkg in sys.modules:
+                del sys.modules[pkg]
 
-    celery._state._apps.discard(
-        app.extensions["invenio-celery"].celery._get_current_object()
-    )
-    celery._state._on_app_finalizers = set()
-    celery._state.set_default_app(Celery())
-
-    # Clear our modules to get them re-imported by Celery.
-    pkgs = [
-        "bpackage.first_tasks",
-        "bpackage.second_tasks",
-        "apackage.third_tasks",
-    ]
-    for pkg in pkgs:
-        if pkg in sys.modules:
-            del sys.modules[pkg]
+    request.addfinalizer(set_celery_state)
+    return app
