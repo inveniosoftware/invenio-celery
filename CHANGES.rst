d796d6aa9e5bd6d5d36c1c678d209fa1d9b3b87c
Alex Ioannidis <a.ioannidis@cern.ch>
2024-03-05 11:27:35 +0100

    release: v1.3.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,12 +1,16 @@
 ..
     This file is part of Invenio.
-    Copyright (C) 2015-2020 CERN.
+    Copyright (C) 2015-2024 CERN.
 
     Invenio is free software; you can redistribute it and/or modify it
     under the terms of the MIT License; see LICENSE file for more details.
 
 Changes
 =======
+
+Version 1.3.0 (released 2024-03-05)
+
+- installation: bump celery to support python 3.11
 
 Version 1.2.5 (released 2022-10-03)
 
--- invenio_celery/__init__.py
+++ invenio_celery/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2015-2024 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -90,6 +90,6 @@ from __future__ import absolute_import, print_function
 
 from .ext import InvenioCelery
 
-__version__ = "1.2.5"
+__version__ = "1.3.0"
 
 __all__ = ("__version__", "InvenioCelery")
